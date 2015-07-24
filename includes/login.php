362c54525f930b9ce74c630fb49eba7e1c73fce7
hnnlrgvrts <hannelore.goovaerts@gmail.com>
2015-07-24 13:22:30 +0200

    Session - Profile - Logout

--- includes/login.php
+++ includes/login.php
@@ -1,9 +1,37 @@
 <?php
-session_start();
-// Do Something
+session_start(); // Starting Session
+$error=''; // Variable To Store Error Message
+if (isset($_POST['submit'])) {
+if (empty($_POST['nickname']) || empty($_POST['password'])) {
+$error = "nickname or Password is invalid";
+}
+else
+{
+// Define $nickname and $password
+$nickname=$_POST['nickname'];
+$password=$_POST['password'];
+// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+$connection = mysql_connect("localhost", "root", "root");
+// To protect MySQL injection for Security purpose
+$nickname = stripslashes($nickname);
+$password = stripslashes($password);
+$nickname = mysql_real_escape_string($nickname);
+$password = mysql_real_escape_string($password);
+// Selecting Database
+$db = mysql_select_db("PHPhub", $connection);
+// SQL query to fetch information of registerd users and finds user match.
+$query = mysql_query("select * from login where password='$password' AND nickname='$nickname'", $connection);
+$rows = mysql_num_rows($query);
+if ($rows == 1) {
+$_SESSION['login_user']=$nickname; // Initializing Session
+header("location: profile.php"); // Redirecting To Other Page
+} else {
+$error = "nickname or Password is invalid";
+}
+mysql_close($connection); // Closing Connection
+}
+}
 ?>
-
-	
  
 
 <!DOCTYPE html>
@@ -15,7 +43,7 @@ session_start();
 <body>
 	
 	<h1>Login</h1>
-	<?php if($res != "") echo $res; ?>
+	
 	<form action="" method="post">
 		
 		<label for="nickname">nickname: </label>
