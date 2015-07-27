a19646bb983c4e6cd6cc72a6d8765141ab2cef9e
hnnlrgvrts <hannelore.goovaerts@gmail.com>
2015-07-27 15:21:31 +0200

    Volledige login en header. ayiss.

--- includes/header.php
+++ includes/header.php
@@ -4,66 +4,62 @@
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
             </button>
             <a class="navbar-brand" href="#">PHP Hub</a>
         </div>
-
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a>
+                <li class="active"><a href="#">Home<span class="sr-only">(current)</span></a>
+            </li>
+            <?php if(!isset($_SESSION['loggedin_user'])):?>
+            <li><a href="includes/login.php">Log In</a>
+        </li>
+        <?php else: ?>
+        <li><a href="includes/logout.php">
+            Logout
+        </a></li>
+        <?endif;?>
+        
+        <li><a href="includes/signup.php">
+            Sign Up!
+        </a></li>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">
+                <?php
+                session_start();
+                if (isset($_SESSION['loggedin_user'])) {
+                echo $_SESSION['loggedin_user'];
+                echo $_SESSION['loggedin_role'];
+                } else {
+                echo 'banaan';
+                }
+                ?>
+            </a></li>
+            <?php if($_SESSION['loggedin_role']==1):?>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Add new project</a>
                 </li>
-                <li><a href="#">Link</a>
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a>
-                        </li>
-                        <li><a href="#">Another action</a>
-                        </li>
-                        <li><a href="#">Something else here</a>
-                        </li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a>
-                        </li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">One more separated link</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-            <form class="navbar-form navbar-left" role="search">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Search">
-                </div>
-                <button type="submit" class="btn btn-default">Submit</button>
-            </form>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Link</a>
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a>
-                        </li>
-                        <li><a href="#">Another action</a>
-                        </li>
-                        <li><a href="#">Something else here</a>
-                        </li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-        <!-- /.navbar-collapse -->
-    </div>
-    <!-- /.container-fluid -->
+                <li><a href="#">Edit Admins</a>
+            </li>
+            <li><a href="#">Something else here</a>
+        </li>
+        <li role="separator" class="divider"></li>
+        <li><a href="#">Separated link</a>
+    </li>
+</ul>
+</li>
+<?php endif; ?>
+</ul>
+</div>
+<!-- /.navbar-collapse -->
+</div>
+<!-- /.container-fluid -->
 </nav>
 <!-- End of navbar -->
--- includes/login.php
+++ includes/login.php
@@ -1,61 +1,66 @@
-<?php 
-
+<?php
+ini_set('display_errors', 1); error_reporting(~0);
 	if (!empty($_POST)) {
 		$salt = "DMIqsegmiF§MEIfjé2";
 		$nickname = $_POST['nickname'];
 		$password = $_POST['password'];
-
 		// connectie maken met database
 		// @ gebruiken om fouten onbeschikbaar te maken
 		$conn = new mysqli("localhost", "root", "root", "PHPhub");
 		if (!$conn->connect_errno) {
 			//connectie is ok
-			$query = "SELECT * FROM users WHERE nickname = '".$conn->real_escape_string($nickname)."';";
-			$result = $conn->query($query);
-			// echo $query;
-			// check of password_verify(wachtwoord) == hash
-			$row_hash = $result->fetch_array();
-			if (password_verify($password, $row_hash['password'])) 
+			$query = "SELECT * FROM db_users WHERE nickname = '".$conn->real_escape_string($nickname)."';";
+			// $result haalt data uit databank 
+			$result=$conn->query($query);
+			// $row_user maakt de data leesbaar (fetch_assoc()).
+			$row_user =$result->fetch_assoc();
+			var_dump($row_user);
+			// als $password in databank staat:
+			if (password_verify($password, $row_user['password']))
 			{
 				echo "Welcome!";
+				// Start de session als login succesvol is!
+				session_start();
+				if (!isset($_SESSION['loggedin_user'])) {
+					$_SESSION['loggedin_user'] = $row_user['nickname'];
+					$_SESSION['loggedin_role'] = $row_user['role'];
+				} else {
+					var_dump($_SESSION);
+				}
+				// redirect naar indexpagina terwijl men aangemeld is. 
+				header("Location: ../index.php");
+				
 			}
 			else
 			{
 				echo "Go away!";
 			}
+	
 		}
+		
 	}
-
 	/*
-
 	
 	
 	*/
-
 	
- ?>
-
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Login</title>
-</head>
-<body>
-	
-	<h1>Login</h1>
-
-	<form action="" method="post">
+	<head>
+		<meta charset="UTF-8">
+		<title>Login</title>
+	</head>
+	<body>
 		
-		<label for="nickname">nickname: </label>
-		<input type="text" id="nickname" name="nickname">
-
-		<label for="password">Password: </label>
-		<input type="password" id="password" name="password">
-
-		<button type="submit">Login!</button>
-
-	</form>
-
-</body>
+		<h1>Login</h1>
+		<form action="" method="post">
+			
+			<label for="nickname">nickname: </label>
+			<input type="text" id="nickname" name="nickname">
+			<label for="password">Password: </label>
+			<input type="password" id="password" name="password">
+			<button type="submit">Login!</button>
+		</form>
+	</body>
 </html>
