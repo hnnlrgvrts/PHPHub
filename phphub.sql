038cf660d7c0479580645de29e75389ae9eab35e
hnnlrgvrts <hannelore.goovaerts@gmail.com>
2015-08-11 17:18:35 +0200

    Updated db.

--- phphub.sql
+++ phphub.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Aug 10, 2015 at 08:43 PM
+-- Generation Time: Aug 11, 2015 at 03:18 PM
 -- Server version: 5.5.41-log
 -- PHP Version: 5.6.8
 
@@ -33,14 +33,15 @@ CREATE TABLE IF NOT EXISTS `db_project` (
   `project_description` varchar(1000) NOT NULL,
   `project_picture` varchar(255) NOT NULL,
   `img_name` varchar(3000) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data for table `db_project`
 --
 
 INSERT INTO `db_project` (`id`, `project_name`, `project_company`, `project_description`, `project_picture`, `img_name`) VALUES
-(10, 'Google', '', 'Google is een zoekmachine enzo. ', '', '');
+(10, 'Google', '', 'Google is the biggest search engine of the world wide web.', '', ''),
+(11, 'PHP Hub', 'Han''s fictional company', 'A project consisting of various different aspects to learn about PHP.', '', '');
 
 -- --------------------------------------------------------
 
@@ -54,15 +55,16 @@ CREATE TABLE IF NOT EXISTS `db_request` (
   `request` varchar(3000) NOT NULL,
   `id_user` int(11) NOT NULL,
   `score` int(11) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=27 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=29 ;
 
 --
 -- Dumping data for table `db_request`
 --
 
 INSERT INTO `db_request` (`id`, `id_project`, `request`, `id_user`, `score`) VALUES
-(25, 10, 'Request 1', 3, 0),
-(26, 10, 'Request 2', 9, 0);
+(25, 10, 'Actually being able to search', 3, 0),
+(26, 10, 'Change the color scheme', 4, 0),
+(27, 10, 'Search by sound sample', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -77,7 +79,7 @@ CREATE TABLE IF NOT EXISTS `db_users` (
   `email` varchar(255) NOT NULL,
   `picture` varchar(255) NOT NULL,
   `role` int(3) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `db_users`
@@ -87,13 +89,7 @@ INSERT INTO `db_users` (`id`, `nickname`, `password`, `email`, `picture`, `role`
 (1, 'hnnlrgvrts', '$2y$12$xGAYXk7yuZYdHYeFKxUop.wBjKrInKYZDDhvHcXchjqtyA0Mtnra2', '', '', 1),
 (2, 'test', '$2y$12$dWT7fAhWX5w3SIfiP6LOv.DzvfY4uCM3TjASQyuxrK1w/kTuFB2n6', '', '', 0),
 (3, 'Guades', '$2y$12$Kexdq.KwvGhedeb2/47ULudVttVrpHBrTxG.kU2hJNXYKT5E8t2p2', '', '', 0),
-(4, 'konijn', '$2y$12$x.n2e9vgZBCj7C8sLOUHvOlhin7dQMeHjeaYakG9B8L.RFPREQrLK', '', '', 0),
-(5, 'Adolf', '$2y$12$dVzjLikKZczar7Q7RtrK.O2AOEpX4TmAQlsvsUPyRtfvOuK7Cbq6e', '', '', 0),
-(6, 'Stalin', '$2y$12$SPBA5leBZjflOBRFbAiH1u3W/XI0wogfw2Ki4VTzP1HFxNk99H3h.', '', '', 0),
-(7, 'Lenin', '$2y$12$OZA8uxy8ZmTZYt0y/s61Te2FC4YpbUATsu/mPbtiPd8jqNHaWp/T.', '', '', 0),
-(8, 'Lenin', '$2y$12$4jh379SI9bs8VNvtmox1xuG.AVwuU4Fj7ymaYzHSc6.ZtZro2RcSW', '', '', 0),
-(9, 'Chegev', '$2y$12$h0QVbXKAwEWX.tSYA2FjcOvxATWLxWy/MqAjdI4aSFHzg8iK4znOa', '', '', 0),
-(10, 'Chegev', '$2y$12$QgOHQJtKwVqxC50bCixgfuI9mt1RBM3aIdRYCRz5HpeHUWUnxcq8K', '', '', 0);
+(4, 'Dreamonic', '$2y$12$3KC1oPks44yWiCvIqaguYurWdrAMPPlJONIRej7QMtKiWtkZ2pRw2', '', '', 0);
 
 -- --------------------------------------------------------
 
@@ -144,17 +140,17 @@ ALTER TABLE `db_voting`
 -- AUTO_INCREMENT for table `db_project`
 --
 ALTER TABLE `db_project`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT for table `db_request`
 --
 ALTER TABLE `db_request`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=27;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=29;
 --
 -- AUTO_INCREMENT for table `db_users`
 --
 ALTER TABLE `db_users`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `db_voting`
 --
